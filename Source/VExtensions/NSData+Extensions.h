83b070e0f4f7bd6f63c568935c549f4ab45edc6a
HeYuan <heyuan@ffrj.net>
2014-07-19 19:14:10 +0800

    新增对文件md5的校验

---
+++ Source/VExtensions/NSData+Extensions.h
@@ -0,0 +1,15 @@
+//
+//  NSData+Extensions.h
+//  VExtensions
+//
+//  Created by yuan on 14-7-19.
+//  Copyright (c) 2014年 www.heyuan110.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (NSData_Extensions)
+
+- (NSString *)MD5;
+
+@end
---
+++ Source/VExtensions/NSData+Extensions.m
@@ -0,0 +1,31 @@
+//
+//  NSData+Extensions.m
+//  VExtensions
+//
+//  Created by yuan on 14-7-19.
+//  Copyright (c) 2014年 www.heyuan110.com. All rights reserved.
+//
+
+#import "NSData+Extensions.h"
+#import <CommonCrypto/CommonDigest.h>
+
+@implementation NSData(NSData_Extensions)
+
+- (NSString*)MD5
+{
+    // Create byte array of unsigned chars
+    unsigned char md5Buffer[CC_MD5_DIGEST_LENGTH];
+    
+    // Create 16 byte MD5 hash value, store in buffer
+    CC_MD5(self.bytes, self.length, md5Buffer);
+    
+    // Convert unsigned char buffer to NSString of hex values
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
+        [output appendFormat:@"%02x",md5Buffer[i]];
+    
+    return output;
+}
+
+
+@end
