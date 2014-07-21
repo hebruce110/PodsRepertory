b5423d437a642b4fec66440d6d1d2785670117b7
HeYuan <heyuan@ffrj.net>
2014-07-21 15:50:04 +0800

    UPDATE

--- Source/VWebService/VRequestManager.h
+++ Source/VWebService/VRequestManager.h
@@ -21,6 +21,7 @@
 #define kRequestMethodDelete                              @"DELETE"
 
 /* 如果你用到了下面的配置，请将响应的定义拷贝到，工程的-Prefix.pch文件中
+默认都是按照约定的格式请求和返回，超时时间也是默认
  
 #define VWEBSERVICE_REQUEST_TIMEOUT 30                     //设置超时时间
 #define VWEBSERVICE_REQUEST_NOT_FORMATTING_CONVENTIONS      //不是按照约定的请求参数格式
